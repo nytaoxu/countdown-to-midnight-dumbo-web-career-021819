5e8b5b532d8541e18d472adc746c0e89727f0af9
William Xu <nytaoxu@gmail.com>
2019-02-08 20:40:47 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,29 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while number > 0
+    sleep 1
+    number -= 1
+  end
+end
+
+# def f(x)
+#   puts x
+# end
+
+# uvw = 15
+
+# def g
+#   f(5)
+#   puts uvw
+# end
+
+# g
